443eaf887546d10b2bb25e95bb5dd35bfafc587e
vtdteam <vtd@vtd.finance>
2021-01-25 09:16:53 +0800

    update dashboard

---
+++ app/precache-manifest.5c252346e7884747f5bb581408ed44a4.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d41672d3367024ac748562390f10b0d1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "37bd0185d93f9e7efb16",
+    "url": "./static/css/3.1b495fff.chunk.css"
+  },
+  {
+    "revision": "1966915ec5d5c97a03b9",
+    "url": "./static/js/0.20cddad4.chunk.js"
+  },
+  {
+    "revision": "ef9a693f1656885b4146",
+    "url": "./static/js/10.14a86eb0.chunk.js"
+  },
+  {
+    "revision": "37bd0185d93f9e7efb16",
+    "url": "./static/js/3.2ea6e0eb.chunk.js"
+  },
+  {
+    "revision": "ba943e391c8155363833d50c974a15c4",
+    "url": "./static/js/3.2ea6e0eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d89992cb08cac7d8dc7f",
+    "url": "./static/js/4.04f72a3e.chunk.js"
+  },
+  {
+    "revision": "800c8c88b71490b35d9e636bbf8c583d",
+    "url": "./static/js/4.04f72a3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94f3a080c78bf8afa064",
+    "url": "./static/js/5.f8b77b9b.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "./static/js/5.f8b77b9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70a5839bab968e2d7cb3",
+    "url": "./static/js/6.6ce88494.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "./static/js/6.6ce88494.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e9dadf53ebcc8337811",
+    "url": "./static/js/7.b4630858.chunk.js"
+  },
+  {
+    "revision": "33dcd919ee64a931a02d",
+    "url": "./static/js/8.b0b8ad53.chunk.js"
+  },
+  {
+    "revision": "43a3b9ad633863033d47",
+    "url": "./static/js/9.21b4423b.chunk.js"
+  },
+  {
+    "revision": "d43bcb65c23262ca7bd991f5625a0c00",
+    "url": "./static/js/9.21b4423b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a98112e7d6fac0d8d2a",
+    "url": "./static/js/main.86813b50.chunk.js"
+  },
+  {
+    "revision": "21a91fcc31a152a3cf09",
+    "url": "./static/js/runtime-main.d6fc2709.js"
+  },
+  {
+    "revision": "a770b6797b68e3f8920e473eb824bac0",
+    "url": "./static/media/loader-big.a770b679.gif"
+  },
+  {
+    "revision": "12f0820c451bdc75f4d1ef97732bf6e8",
+    "url": "./static/media/rw-widgets.12f0820c.woff"
+  },
+  {
+    "revision": "792dcd18baf5f544aabcad1883d673c2",
+    "url": "./static/media/rw-widgets.792dcd18.svg"
+  },
+  {
+    "revision": "bc7c4a59f924cf037aad6e1f9edba366",
+    "url": "./static/media/rw-widgets.bc7c4a59.eot"
+  },
+  {
+    "revision": "eceddf474df95d8d4a7e316668c3be85",
+    "url": "./static/media/rw-widgets.eceddf47.ttf"
+  }
+]);
--- app/service-worker.js
+++ app/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.e66e3b5520f1bb51fbf211a14646aed7.js"
+  "./precache-manifest.5c252346e7884747f5bb581408ed44a4.js"
 );
 
 self.addEventListener('message', (event) => {
